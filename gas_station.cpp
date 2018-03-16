0bfe3be82ef1338d0dfe44095a4ac92caf8041ca
Adam Nelson <adamnelson@mines.edu>
2018-03-16 14:12:24 -0600

    Added the base C++ file

---
+++ gas_station.cpp
@@ -0,0 +1,30 @@
+/*
+ * Dynamic Programming Gas Station Problem
+ * Authors: Adam Nelson, Akshay Swaminathan, Lewis Setter
+ * Date: 2018-03-16
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+/*
+ * Function Prototypes
+ */
+
+
+
+int main() {
+	//Read n, L, P, c from input file
+	
+
+
+	return 0;
+}
+
+
+/*
+ * Functions
+ */
+
