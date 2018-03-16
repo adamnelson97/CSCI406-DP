fa0e6ea37f5a8a75c36752f3ea6ce86adb9d6125
Adam Nelson <adamnelson@mines.edu>
2018-03-16 14:17:56 -0600

    Added code to store n, L, P, c from input file.

--- gas_station.cpp
+++ gas_station.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 /*
@@ -17,7 +18,30 @@ using namespace std;
 
 int main() {
 	//Read n, L, P, c from input file
-	
+	string filename;
+	cout << "Enter the name of the input file (no .txt): ";
+	cin >> filename;
+	filename += ".txt";
+
+	ifstream input(filename.c_str()); //Open the filestream
+	if (!input.is_open()) { //Test that the file opened properly
+		cerr << "Unable to open file " << filename << endl;
+		return -1;
+	}
+
+	int n, L, P, c;
+	input >> n;
+	input >> L;
+	input >> P;
+	input >> c;
+
+	/*
+	 * Store the gas requirements for each day. Since the number of days may
+	 * change with inputs, we cannot use an array since their size is static.
+	 */
+
+
+
 
 
 	return 0;
